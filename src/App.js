2a07e921c482e9ce8e77256fd9259ecceec6a594
TheRealRondon <amirhossein.abaskohi@gmail.com>
2022-04-25 18:35:22 +0430

    Checking bootstrap classes/

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="badge badge-primary">
           Edit <code>src/App.js</code> and save to reload. This is coffe: <FontAwesomeIcon icon={faCoffee} />
         </p>
         <a
