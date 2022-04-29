9e665ae3c76cba9427b592eeac0aa73fb7277c1e
TheRealRondon <amirhossein2324236@gmail.com>
2022-04-29 19:02:59 +0430

    Added routing.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import Login from './login';
 import Movies from './movies';
 import Signup from './signup';
-import root from './tools';
 import Movie from './movie';
 import Actor from './actor';
 import Watchlist from './watchlist';
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from "react-router-dom";
 
-root.render(
-  <Movies/>
-);
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Movies /> },
+    { path: "/movies", element: <Movies /> },
+    { path: "/watchlist", element: <Watchlist /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <Signup /> }
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
+render(<AppWrapper />, document.getElementById("root"));
